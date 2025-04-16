0e8dc1607e43a6510b23d7b320e26c9fc9fe101b
AvanthikaRamachandiran <147021780+AvanthikaRamachandiran@users.noreply.github.com>
2025-04-16 20:06:46 +0530

    Day7

---
+++ casting.java
@@ -0,0 +1,26 @@
+package day7;
+
+public class casting {
+	public static void main(String[] args) {
+		System.out.println(" implicit value");
+		byte b= 127;
+		short s=(short)b;
+		System.out.println(b);
+		int num=10;
+		long lnum=num;
+		System.out.println(lnum);
+		//implicit
+		System.out.println(" explicit value");
+		short S=128;
+		byte B=(byte)S;
+	
+		
+		System.out.println(B);
+		long a=230;
+		int i=(int)a;
+		System.out.println(i);
+		
+		
+	}
+
+}
